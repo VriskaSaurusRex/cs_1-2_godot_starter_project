788b5463dd0453e68e96fb2b11871a0ccac5e72e
VriskaSaurusRex <elhiestand8164@student.pps.net>
2025-10-03 09:59:27 -0700

    I HATE MY CODE AND MYSELF HELP ME

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,21 +1,30 @@
 extends CharacterBody2D
-
-
+var max_time=2
+var time=max_time
+var in_range=false
+var player
 func _ready():
 	
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if in_range:
+		time-=delta
+		if time<0:
+			
+			time=max_time
 
 
 	
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	player=body
+	if body.name=="Player":
+		in_range=true
 	
-	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name=="Player":
+		in_range=false
